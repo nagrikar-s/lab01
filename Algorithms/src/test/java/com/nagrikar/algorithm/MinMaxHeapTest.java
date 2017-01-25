a0fbf67f3dadb627a0f73e15912c1635ca7c50aa
sneha <snagrikar358@gmail.com>
2017-01-24 16:57:55 -0800

    minor changes

--- Algorithms/src/test/java/com/nagrikar/algorithm/MinMaxHeapTest.java
+++ Algorithms/src/test/java/com/nagrikar/algorithm/MinMaxHeapTest.java
@@ -15,43 +15,42 @@ public class MinMaxHeapTest {
 
     @Test
     public void testBuildHeap() {
-        assertEquals(newArrayList(1, 3, 2), new MinMaxHeap<Integer>(new Integer[]{2, 3, 1}, 3, MINHEAP).toList());
+        assertEquals(newArrayList(1, 3, 2), new MinMaxHeap<>(new Integer[]{2, 3, 1}, 3, MINHEAP).toList());
     }
-
 
     @Test
     public void testInsert() {
-        assertEquals(newArrayList(1, 3, 2, 4), new MinMaxHeap(new Integer[]{2, 3, 1}, 4, MINHEAP).
-                insert(4).toList());
-        assertEquals(newArrayList(-1, 1, 2, 4, 3), new MinMaxHeap(new Integer[]{1, 3, 2, 4}, 5, MINHEAP).insert(-1)
+        assertEquals(newArrayList(1, 3, 2, 4), new MinMaxHeap<>(new Integer[]{2, 3, 1}, 4, MINHEAP).insert(4).toList());
+        assertEquals(newArrayList(-1, 1, 2, 4, 3), new MinMaxHeap<>(new Integer[]{1, 3, 2, 4}, 5, MINHEAP).insert(-1)
                 .toList());
     }
 
     @Test
     public void testDelete() {
-        assertEquals(newArrayList(2, 3), new MinMaxHeap(new Integer[]{2, 3, 1}, 3, MINHEAP).delete().toList());
-        assertEquals(newArrayList(1L, 3L, 2L, 4L), new MinMaxHeap(new Long[]{-1l, 1l, 2l, 4l, 3l}, 5, MINHEAP).delete
-                ().toList());
+        assertEquals(newArrayList(2, 3), new MinMaxHeap<>(new Integer[]{2, 3, 1}, 3, MINHEAP).delete().toList());
+        assertEquals(newArrayList(1L, 3L, 2L, 4L), new MinMaxHeap<>(new Long[]{-1L, 1L, 2L, 4L, 3L}, 5, MINHEAP)
+                .delete().toList());
     }
 
 
     @Test
     public void testBuildMaxHeap() {
-        assertEquals(newArrayList(3, 2, 1), new MinMaxHeap(new Integer[]{2, 3, 1}, 3, MAXHEAP).toList());
+        assertEquals(newArrayList(3, 2, 1), new MinMaxHeap<>(new Integer[]{2, 3, 1}, 3, MAXHEAP).toList());
     }
 
     @Test
     public void testInsertMaxHeap() {
-        assertEquals(newArrayList(4, 3, 1, 2), new MinMaxHeap(new Integer[]{3, 2, 1}, 4, MAXHEAP).insert(4).toList());
-        assertEquals(newArrayList(4, 3, 1, 2, -1), new MinMaxHeap(new Integer[]{4, 3, 1, 2}, 5, MAXHEAP).insert(-1)
+        assertEquals(newArrayList(4, 3, 1, 2), new MinMaxHeap<>(new Integer[]{3, 2, 1}, 4, MAXHEAP).insert(4)
                 .toList());
+        assertEquals(newArrayList(4, 3, 1, 2, -1), new MinMaxHeap<>(new Integer[]{4, 3, 1, 2}, 5, MAXHEAP)
+                .insert(-1).toList());
 
     }
 
     @Test
     public void testDeleteMaxHeap() {
-        assertEquals(newArrayList(2, 1), new MinMaxHeap(new Integer[]{3, 2, 1}, 3, MAXHEAP).delete().toList());
-        assertEquals(newArrayList(3, 2, 1, -1), new MinMaxHeap(new Integer[]{4, 3, 1, 2, -1}, 5, MAXHEAP).delete()
-                .toList());
+        assertEquals(new MinMaxHeap<>(new Integer[]{3, 2, 1}, 3, MAXHEAP).delete().toList(), newArrayList(2, 1));
+        assertEquals(newArrayList(3, 2, 1, -1), new MinMaxHeap<>(new Integer[]{4, 3, 1, 2, -1}, 5, MAXHEAP)
+                .delete().toList());
     }
 }
